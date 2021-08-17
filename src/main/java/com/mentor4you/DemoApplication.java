aec81536fbb7d4c9de111792228e5e7408b09faa
zbohdan <zbogdan349@gmail.com>
2021-08-17 19:27:27 +0300

    add Swagger

--- src/main/java/com/mentor4you/DemoApplication.java
+++ src/main/java/com/mentor4you/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.mentor4you;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Tag(name = "mentor4you", description = "Test description")
 @RestController
 public class DemoApplication {
 
@@ -14,6 +18,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+	@Operation(summary = "Test method", description = "Returns a Hello World")
 	@GetMapping("/")
 	public String hello(@RequestParam(value = "name", defaultValue = "World!!!") String name) {
 		return String.format("Hello %s!", name);
