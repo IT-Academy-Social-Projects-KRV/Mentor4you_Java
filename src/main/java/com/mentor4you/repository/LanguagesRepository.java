22c35abb3646bbe48906809ad9ae3a9861dafef1
olexandr256 <olexandr256@gmail.com>
2021-08-29 22:55:32 +0300

    add model Languages

---
+++ src/main/java/com/mentor4you/repository/LanguagesRepository.java
@@ -0,0 +1,9 @@
+package com.mentor4you.repository;
+
+import com.mentor4you.model.Languages;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguagesRepository extends JpaRepository<Languages, Integer> {
+}
