d7b9152c797e28123456ad98b6b8b8877dfbfffa
olexandr256 <olexandr256@gmail.com>
2021-09-07 23:40:42 +0300

    update files - added jwt dependens, configuration

--- src/main/java/com/mentor4you/security/WebSecurityConfig.java
+++ src/main/java/com/mentor4you/security/WebSecurityConfig.java
@@ -1,31 +1,54 @@
 package com.mentor4you.security;
 
+import com.mentor4you.security.jwt.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtFilter jwtFilter;
+
+    public WebSecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 //виключив csrf захист щоб можна було надсилати POST запити
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/testSecurity").authenticated()
-                .anyRequest().permitAll()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .formLogin()
-                .permitAll()
+                .authorizeRequests().antMatchers("/testSecurity",
+                        "/system/testAuth")
+                .authenticated()
+                .anyRequest().permitAll()
+//                .and()
+//                .formLogin()
+//                .permitAll()
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         ;
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
