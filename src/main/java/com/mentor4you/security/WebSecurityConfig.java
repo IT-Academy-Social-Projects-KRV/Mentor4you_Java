a9573233ec500e95f09a9b3d328a2b8735f0c7bd
AlexDRKasianchuk <olexandr.kasianchuk@gmail.com>
2021-09-20 18:20:47 +0300

    fix cors settings

--- src/main/java/com/mentor4you/security/WebSecurityConfig.java
+++ src/main/java/com/mentor4you/security/WebSecurityConfig.java
@@ -32,6 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 //виключив csrf захист щоб можна було надсилати POST запити
                 .csrf().disable()
                 .sessionManagement()
