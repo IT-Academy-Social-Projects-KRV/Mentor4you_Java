740fef7a3348b2a882452c122471609ac2a37f1c
alehandromaniuk <57686519+alehandromaniuk@users.noreply.github.com>
2021-09-21 22:36:30 +0300

    Reset password

---
+++ src/main/java/com/mentor4you/model/DTO/SecureLoginDTO.java
@@ -0,0 +1,15 @@
+package com.mentor4you.model.DTO;
+
+public class SecureLoginDTO {
+
+    private String email;
+    private String securetoken;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSecuretoken() {
+        return securetoken;
+    }
+}
