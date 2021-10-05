01e01ad642167c9b70c52272852c2c6914bf8739
AlexDRKasianchuk <olexandr.kasianchuk@gmail.com>
2021-10-05 21:02:41 +0300

    chat && add role to token && add path to CORS(delete after tests)

--- src/main/java/com/mentor4you/controller/ChatRoomController.java
+++ src/main/java/com/mentor4you/controller/ChatRoomController.java
@@ -3,6 +3,7 @@ package com.mentor4you.controller;
 import com.mentor4you.model.ChatMessage;
 import com.mentor4you.model.ChatRoom;
 import com.mentor4you.repository.ChatRoomRepository;
+import com.mentor4you.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -10,6 +11,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -18,9 +20,17 @@ import java.util.Optional;
 public class ChatRoomController {
 
     @Autowired
-    private ChatRoomRepository chatRoomRepository;
-    @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    ChatRoomRepository chatRoomRepository;
+    ChatService chatService;
+
+    public ChatRoomController(ChatRoomRepository chatRoomRepository, SimpMessagingTemplate simpMessagingTemplate, ChatService chatService) {
+        this.chatRoomRepository = chatRoomRepository;
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.chatService = chatService;
+    }
 
     @MessageMapping("/{to}")
     public void sendMessage(@DestinationVariable String to, ChatMessage message) {
@@ -34,17 +44,15 @@ public class ChatRoomController {
         return ResponseEntity.ok(chatRoomRepository.findAll());
     }
 
-//    //add new Chat room
-//        @PostMapping("/{id}")
-//    ResponseEntity<?> createRoom(@PathVariable String id) {
-//            String senderId = "1";
-//            Optional<ChatRoom> roomOptional = chatRoomRepository.findBySenderIdAndRecipientId(senderId, id);
-//            Optional<ChatRoom> reverseRoomOptional = chatRoomRepository.findBySenderIdAndRecipientId(id, senderId);
-//            if (roomOptional.isEmpty() | reverseRoomOptional.isEmpty()) {
-//                String chatId = String.format("%s_%s", senderId, id);
-//                ChatRoom room = new ChatRoom(chatId, senderId, id ,"name","Avatar_url");
-//                return ResponseEntity.status(201).body(chatRoomRepository.save(room));
-//            }
-//            return ResponseEntity.status(201).body(reverseRoomOptional);
+    //get or add new Chat room
+    @PostMapping("/{id}")
+    ResponseEntity<?> getRoom(HttpServletRequest request, @PathVariable String id) {
+//        try {
+//            chatService.getRoom(request,id);
+//            return ResponseEntity.ok("me");
+//        }catch (NullPointerException e){
+            return ResponseEntity.ok(chatService.createRoom(request, id));
 //        }
+
+        }
 }
