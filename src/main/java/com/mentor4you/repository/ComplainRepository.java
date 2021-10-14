2f6625815abd3b94b8be46597f47b0c846d5b8cb
AndriyGu <andriy.gumich@gmail.com>
2021-10-14 13:03:38 +0300

    NewTestSql

--- src/main/java/com/mentor4you/repository/ComplainRepository.java
+++ src/main/java/com/mentor4you/repository/ComplainRepository.java
@@ -2,7 +2,13 @@ package com.mentor4you.repository;
 
 import com.mentor4you.model.Complaint;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ComplainRepository extends MongoRepository<Complaint, String> {
+
+
 
 }
