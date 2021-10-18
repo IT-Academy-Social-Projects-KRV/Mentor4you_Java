419640f16bdedc6318728031792dd2e2e11d995f
AndriyGu <andriy.gumich@gmail.com>
2021-10-18 11:58:10 +0300

    NewTestSql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,28 @@
 INSERT INTO mentor4you_db.users (avatar, email, first_name, last_name, password, registration_date, role, status) VALUES
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/adminAvatar.jpeg','Admin1@gmail.com', 'Admin', 'Admino', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'ADMIN',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/moderatorAvatar.jpg','Moderator1@gmail.com', 'Moder', 'Mod', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MODERATOR',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/moderatorAvatar.jpg','Moderator2@gmail.com', 'Mod', 'Moder', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MODERATOR',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/moderatorAvatar.jpg','Moderator3@gmail.com', 'Modest', 'Moderovich', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MODERATOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Admin1@gmail.com', 'Admin', 'Admino', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'ADMIN',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Moderator1@gmail.com', 'Moder', 'Mod', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MODERATOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Moderator2@gmail.com', 'Mod', 'Moder', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MODERATOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Moderator3@gmail.com', 'Modest', 'Moderovich', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MODERATOR',true),
 
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor1@gmail.com', 'Andrii', 'Android', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor2@gmail.com', 'Mykola', 'Myhalkov', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor3@gmail.com', 'Vasyl', 'Vasyliv', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor4@gmail.com', 'Ivan', 'Ivanov', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor5@gmail.com', 'Leonard', 'Vasyliv', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor1@gmail.com', 'Andrii', 'Android', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor2@gmail.com', 'Mykola', 'Myhalkov', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor3@gmail.com', 'Vasyl', 'Vasyliv', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor4@gmail.com', 'Ivan', 'Ivanov', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor5@gmail.com', 'Leonard', 'Vasyliv', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTOR',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor6@gmail.com', 'Eleonora', 'Tovarnjak', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',false ),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor7@gmail.com', 'Jozefina', 'Berdjanskaja', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',false ),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor8@gmail.com', 'Kyril', 'Oblomingo', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',false ),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor9@gmail.com', 'Viniamin', 'Diesel', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',false ),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor10@gmail.com', 'Antuan', 'Rakushnjak', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',false ),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor11@gmail.com', 'Eldar', 'Paskalchenko', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',false ),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentor12@gmail.com', 'Maria', 'Los Popados', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTOR',false ),
 
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee1@gmail.com', 'Orest', 'Knyazev', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTEE',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee2@gmail.com', 'Dmytro', 'Vasiv', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTEE',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee3@gmail.com', 'Bogdan', 'Ivanov', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTEE',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee4@gmail.com', 'Peter', 'Kwit', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTEE',true),
-('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee5@gmail.com', 'Herasim', 'Nechai', '$2a$10$iFfOLYfhGHPURIbw5kWwlepRKL5YCQ9ye7KItSGBrogUn3M9H5RLu', '2021-09-13 10:42:19.236442', 'MENTEE',true);
+
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee1@gmail.com', 'Orest', 'Knyazev', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTEE',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee2@gmail.com', 'Dmytro', 'Vasiv', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTEE',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee3@gmail.com', 'Bogdan', 'Ivanov', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTEE',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee4@gmail.com', 'Peter', 'Kwit', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTEE',true),
+('https://awss3mentor4you.s3.eu-west-3.amazonaws.com/avatars/standartUserAvatar.png','Mentee5@gmail.com', 'Herasim', 'Nechai', '$2a$10$LTIXsz6vEwmL0Bqwr9vDsefnHsi15M99WDuCTY8ejfMzFEGq/25Cy', '2021-09-13 10:42:19.236442', 'MENTEE',true);
 COMMIT;
 
 INSERT INTO mentor4you_db.accounts (id, last_visit) VALUES
@@ -31,24 +39,31 @@ INSERT INTO mentor4you_db.accounts (id, last_visit) VALUES
 (11,'2021-09-13 10:42:19.236442'),
 (12,'2021-09-13 10:42:19.236442'),
 (13,'2021-09-13 10:42:19.236442'),
-(14,'2021-09-13 10:42:19.236442');
+(14,'2021-09-13 10:42:19.236442'),
+(15,'2021-09-13 10:42:19.236442'),
+(16,'2021-09-13 10:42:19.236442'),
+(17,'2021-09-13 10:42:19.236442'),
+(18,'2021-09-13 10:42:19.236442'),
+(19,'2021-09-13 10:42:19.236442'),
+(20,'2021-09-13 10:42:19.236442'),
+(21,'2021-09-13 10:42:19.236442');
 COMMIT;
 
 INSERT INTO mentor4you_db.mentors (id, description, is_offline_in, is_offline_out,
-                                   is_online, showable_status, group_serv,rating) VALUES
-(5, 'Java', 1, 1, 1, 1, 'YES',5),
-(6, 'Web', 1, 1, 1, 1, 'YES',4),
-(7, 'ArtI', 1, 0, 1, 1, 'YES',3),
-(8, 'Design', 0, 1, 1, 1, 'YES',4),
-(9, 'Mobile', 0, 0, 1, 1, 'YES',5);
+                                   is_online, showable_status, group_serv) VALUES
+(5, 'Java', 1, 1, 1, 1, 'YES'),
+(6, 'Web', 1, 1, 1, 1, 'YES'),
+(7, 'ArtI', 1, 0, 1, 1, 'YES'),
+(8, 'Design', 0, 1, 1, 1, 'YES'),
+(9, 'Mobile', 0, 0, 1, 1, 'YES');
 COMMIT;
 
 INSERT INTO mentor4you_db.mentees VALUES
-(10),
-(11),
-(12),
-(13),
-(14);
+(17),
+(18),
+(19),
+(20),
+(21);
 COMMIT;
 
 INSERT INTO mentor4you_db.categories (name) VALUES
@@ -80,7 +95,15 @@ INSERT INTO mentor4you_db.languages_to_accounts (account_id, languages_id) VALUE
 (11,1),(11,2),
 (12,1),(12,2),
 (13,1),(13,2),
-(14,1),(14,2);
+(14,1),(14,2),
+(15,1),(15,2),
+(16,1),(16,2),
+(17,1),(17,2),
+(18,1),(18,2),
+(19,1),(19,2),
+(20,1),(20,2),
+(21,1),(21,2);
+
 COMMIT;
 
 INSERT INTO mentor4you_db.type_contacts (id, name) VALUES
@@ -95,22 +118,22 @@ INSERT INTO mentor4you_db.type_contacts (id, name) VALUES
 COMMIT;
 
 INSERT INTO mentor4you_db.contacts_to_accounts (contact_data, accounts_id, type_contacts_id) VALUES
-('+380998877666', 1, 1),  ('My-site URL', 1, 3), ('My GitHub URL',1, 7),
-('+380998877667', 2, 1),
-('+380998877668', 3, 1),
-('+380998877669', 4, 1),
+('+380998877666', 1, 1), ('Admin1@gmail.com', 1, 8), ('My-site URL', 1, 3), ('My GitHub URL',1, 7),
+('+380998877667', 2, 1), ('Moderator1@gmail.com',2,8),
+('+380998877668', 3, 1), ('Moderator2@gmail.com',3,8),
+('+380998877669', 4, 1), ('Moderator3@gmail.com',4,8),
 
-('+380998877670', 5, 1),('My-site URL', 5, 3), ('My GitHub URL',5, 7),
-('+380998877671', 6, 1),('My-site URL', 6, 3), ('My GitHub URL',6, 7),
-('+380998877672', 7, 1),('My-site URL', 7, 3), ('My GitHub URL',7, 7),
-('+380998877673', 8, 1),('My-site URL', 8, 3), ('My GitHub URL',8, 7),
-('+380998877674', 9, 1),('My-site URL', 9, 3), ('My GitHub URL',9, 7),
+('+380998877670', 5, 1), ('Mentor1@gmail.com', 5, 8), ('My-site URL', 5, 3), ('My GitHub URL',5, 7),
+('+380998877671', 6, 1), ('Mentor2@gmail.com', 6, 8), ('My-site URL', 6, 3), ('My GitHub URL',6, 7),
+('+380998877672', 7, 1), ('Mentor3@gmail.com', 7, 8), ('My-site URL', 7, 3), ('My GitHub URL',7, 7),
+('+380998877673', 8, 1), ('Mentor4@gmail.com', 8, 8), ('My-site URL', 8, 3), ('My GitHub URL',8, 7),
+('+380998877674', 9, 1), ('Mentor5@gmail.com', 9, 8), ('My-site URL', 9, 3), ('My GitHub URL',9, 7),
 
-('+380998877675', 9, 1),('My-site URL', 9, 3), ('My GitHub URL',9, 7),
-('+380998877676', 9, 1),('My-site URL', 9, 3), ('My GitHub URL',9, 7),
-('+380998877677', 9, 1),('My-site URL', 9, 3), ('My GitHub URL',9, 7),
-('+380998877678', 9, 1),('My-site URL', 9, 3), ('My GitHub URL',9, 7),
-('+380998877679', 9, 1),('My-site URL', 9, 3), ('My GitHub URL',9, 7);
+('+380998877675', 17, 1), ('Mentee1@gmail.com', 17, 8), ('My-site URL', 17, 3), ('My GitHub URL',17, 7),
+('+380998877676', 18, 1), ('Mentee2@gmail.com', 18, 8), ('My-site URL', 18, 3), ('My GitHub URL',18, 7),
+('+380998877677', 19, 1), ('Mentee3@gmail.com', 19, 8), ('My-site URL', 19, 3), ('My GitHub URL',19, 7),
+('+380998877678', 20, 1), ('Mentee4@gmail.com', 20, 8), ('My-site URL', 20, 3), ('My GitHub URL',20, 7),
+('+380998877679', 21, 1), ('Mentee5@gmail.com', 21, 8), ('My-site URL', 21, 3), ('My GitHub URL',21, 7);
 COMMIT;
 
 INSERT INTO mentor4you_db.mentors_to_categories (id, currency, rate,categories_id ,mentors_id) VALUES
@@ -131,27 +154,12 @@ COMMIT;
 
 
 INSERT INTO mentor4you_db.certificats (id,description,link,name,certificats) VALUES
-(1,'description','http://link','Very Cool Certificate',5),
-(2,'description','http://link','Very Cool Certificate',6),
-(3,'description','http://link','Very Cool Certificate',7),
-(4,'description','http://link','Very Cool Certificate',8),
-(5,'description','http://link','Very Cool Certificate',9),
-(6,'description','http://link','Very Cool Certificate',5),
-(7,'description','http://link','Very Cool Certificate',8),
-(8,'description','http://link','Very Cool Certificate',9);
-COMMIT;
-
-INSERT INTO mentor4you_db.city (name) VALUES
-('Poltava'),
-('Ostroh'),
-('Kyiv'),
-('Lviv');
-COMMIT;
-
-INSERT INTO mentor4you_db.city_to_mentors (city_id, mentor_id) VALUES
-('3', '5'),
-('1', '6'),
-('3', '7'),
-('2', '8'),
-('4', '9');
+(1,'description','http://link','Very Cool Certificat',5),
+(2,'description','http://link','Very Cool Certificat',6),
+(3,'description','http://link','Very Cool Certificat',7),
+(4,'description','http://link','Very Cool Certificat',8),
+(5,'description','http://link','Very Cool Certificat',9),
+(6,'description','http://link','Very Cool Certificat',5),
+(7,'description','http://link','Very Cool Certificat',8),
+(8,'description','http://link','Very Cool Certificat',9);
 COMMIT;
