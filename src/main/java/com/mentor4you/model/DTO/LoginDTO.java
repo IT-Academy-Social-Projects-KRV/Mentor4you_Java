a090aefba49c2d24841b13d1a0969731f5a7e367
AlexDRKasianchuk <olexandr.kasianchuk@gmail.com>
2021-10-20 13:22:32 +0300

    add test for login

--- src/main/java/com/mentor4you/model/DTO/LoginDTO.java
+++ src/main/java/com/mentor4you/model/DTO/LoginDTO.java
@@ -5,6 +5,14 @@ public class LoginDTO {
     private String email;
     private String password;
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getEmail() {
         return email;
     }
