6816e18406daa31228d4c84b0a745509afe8f033
alehandromaniuk <57686519+alehandromaniuk@users.noreply.github.com>
2021-10-23 17:58:29 +0300

    fix

--- src/main/java/com/mentor4you/security/jwt/CustomUserDetailsService.java
+++ src/main/java/com/mentor4you/security/jwt/CustomUserDetailsService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.lang.model.UnknownEntityException;
 import javax.servlet.http.HttpServletRequest;
 
 @Component
